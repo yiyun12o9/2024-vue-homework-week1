447b5ed00cd7f4ede74f13a219f387249c48bdba
yiyun12o9 <yoyolee7012@gmail.com>
2024-08-06 23:51:39 +0800

    update code

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/2024-vue-homework-week1/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
